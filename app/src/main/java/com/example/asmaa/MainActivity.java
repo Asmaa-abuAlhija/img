1fc866d71a8c6b78434adbc0ce0841ee317044e1
MA7MOS <mahmod1w212@gmail.com>
2024-12-18 13:17:51 +0200

    first

---
+++ app/src/main/java/com/example/asmaa/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.asmaa;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView img;
+    int c=1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img=findViewById(R.id.img);
+    }
+    public void right (View view){
+        if(c==1){
+            img.setImageResource(R.drawable.);
+            c=2;
+        }
+        else{
+            if(c==2){
+                img.setImageResource(R.drawable.);
+                c=3;
+            }
+            else{
+                if(c==3){
+                    img.setImageResource(R.drawable.);
+                    c=4;
+                }
+                else{
+                    if(c==4){
+                        img.setImageResource(R.drawable.);
+                        c=5;
+                    }
+                    else{
+                        img.setImageResource(R.drawable.mi1);
+                        c=1;
+                    }
+                }
+            }
+        }
+    }
+    public void left (View view){
+        if(c==5){
+            img.setImageResource(R.drawable.);
+            c=4;
+        }
+        else{
+            if(c==4){
+                img.setImageResource(R.drawable.);
+                c=3;
+            }
+            else{
+                if(c==3){
+                    img.setImageResource(R.drawable.);
+                    c=2;
+                }
+                else{
+                    if(c==2){
+                        img.setImageResource(R.drawable.mi1);
+                        c=1;
+                    }
+                    else{
+                        img.setImageResource(R.drawable.);
+                        c=5;
+                    }
+                }
+            }
+        }
+    }
+}
